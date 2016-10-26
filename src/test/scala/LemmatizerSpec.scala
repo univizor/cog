4bb1dbc43678c4d5e75b3308917eb3e2fd575919
Oto Brglez <otobrglez@gmail.com>
2016-10-26 15:39:29 +0200

    Ignore slow test in test.

--- src/test/scala/LemmatizerSpec.scala
+++ src/test/scala/LemmatizerSpec.scala
@@ -15,7 +15,7 @@ class LemmatizerSpec extends FunSpec {
       assertResult(true)("(biti)".r.findFirstIn(text).getOrElse("") != "")
     }
 
-    it("ZitnikLemmatizer") {
+    ignore("ZitnikLemmatizer") {
       object L extends ZitnikLemmatizer {}
       val text = L.lemmatize(originalText)
 
