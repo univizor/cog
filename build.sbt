bc2cecd09a6b7d9a7d226047497284faf8b9091b
Oto Brglez <otobrglez@gmail.com>
2016-11-28 23:59:36 +0100

    Adding TF-IDF and KMeans.

--- build.sbt
+++ build.sbt
@@ -22,7 +22,7 @@ libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % sparkVersion,
   "org.apache.spark" %% "spark-sql" % sparkVersion,
   "org.apache.spark" %% "spark-repl" % sparkVersion,
-  // "org.apache.spark" %% "spark-hive" % sparkVersion,
+  "org.apache.spark" %% "spark-mllib" % sparkVersion,
 
   "org.apache.opennlp" % "opennlp-tools" % opennlpVersion,
 
--- src/main/scala/SentenceMatcher.scala
+++ src/main/scala/SentenceMatcher.scala
@@ -1,7 +1,9 @@
 package cog
 
 import org.apache.log4j.{LogManager, Level, Logger}
+import org.apache.spark.ml.clustering.KMeans
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.feature.{HashingTF, IDF, Tokenizer}
 
 trait CogSparkMatcherApp extends App {
   final val logLevel = Level.ERROR
@@ -20,7 +22,42 @@ object SentenceMatcher extends CogSparkMatcherApp {
     val documents = spark.read.load("data/documents")
     documents.createOrReplaceTempView("documents")
 
-    spark.sql("SELECT COUNT(*) from documents").show
+    val tokenizer = new Tokenizer().setInputCol("document").setOutputCol("words")
+
+    val wordsData = tokenizer.transform(documents)
+    val hashingTF = new HashingTF()
+      .setInputCol("words")
+      .setOutputCol("rawFeatures")
+      .setNumFeatures(20)
+
+    val ftdData = hashingTF.transform(wordsData)
+      .cache()
+
+    val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
+    val idfModel = idf.fit(ftdData)
+
+    val rescaledData = idfModel.transform(ftdData)
+      .withColumnRenamed("fileName", "label")
+
+    val kmeans = new KMeans()
+      // .setK(3)
+      .setSeed(1L)
+      .setFeaturesCol("rawFeatures")
+
+    val kmeansModel = kmeans.fit(rescaledData)
+
+    val WSSSE = kmeansModel.computeCost(rescaledData)
+    println(s"Within Set Sum of Squared Errors = $WSSSE")
+
+    kmeansModel.clusterCenters.foreach(println)
+
+    println(s"K = ${kmeansModel.getK}")
+
+    kmeansModel.summary
+      .predictions
+      .select("label", "prediction")
+      .show()
+    // .printSchema()
 
   } finally {
     spark.stop()
