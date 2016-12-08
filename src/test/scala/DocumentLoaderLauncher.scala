046ef30717c4d2009c6794f584db0c7798b8b6e7
Oto Brglez <otobrglez@gmail.com>
2016-12-09 01:05:33 +0800

    Fixing broken test.

--- src/test/scala/DocumentLoaderLauncher.scala
+++
@@ -1,5 +0,0 @@
-object DocumentLoaderLauncher extends App {
-  System.setProperty("spark.master", "local[*]")
-  var newArgs = Array("./data/files/*", "./data/documents")
-  DocumentLoader.main(newArgs)
-}
