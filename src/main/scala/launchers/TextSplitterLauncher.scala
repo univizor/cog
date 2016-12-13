23cf6d908014e89a1dbbaabbb115c26dd0788b66
Oto Brglez <otobrglez@gmail.com>
2016-12-13 22:03:51 +0800

    Speedup for matching.

--- src/main/scala/launchers/TextSplitterLauncher.scala
+++ src/main/scala/launchers/TextSplitterLauncher.scala
@@ -10,8 +10,8 @@ object TextSplitterLauncher extends App {
 
   System.setProperty("spark.master", "local[*]")
 
-  val testArgs = Array[String]("./data/10-files")
-  // val testArgs = Array[String]("./data/files")
+  // val testArgs = Array[String]("./data/10-files")
+  val testArgs = Array[String]("./data/files")
 
   TextSplitter.main(testArgs)
 }
