46b646c1d1c34345a814f54ee907de1cd202dc49
Oto Brglez <otobrglez@gmail.com>
2016-12-21 23:35:56 +0100

    Escaping in query.

--- src/main/scala/SentenceMatcherX.scala
+++ src/main/scala/SentenceMatcherX.scala
@@ -18,7 +18,8 @@ case class Sentence(fileName: String, pageNumber: Int, sentenceIndex: Int, sente
 
 object SentenceMatcherX extends SentenceMatcherXSparkApp {
   final val MIN_SENTENCE_LENGTH = spark.conf.getOption("min_sentence_length").getOrElse("20").toInt
-  final val MAX_SENTENCE_LENGTH = spark.conf.getOption("max_sentence_length").getOrElse("200").toInt
+  final val MAX_SENTENCE_LENGTH = spark.conf.getOption("max_sentence_length").getOrElse("400").toInt
+  final val SENTENCE_FUZZINESS = spark.conf.getOption("sentence_fuzziness").getOrElse("0.99").toFloat
 
   val logger = LogManager.getLogger("SentenceMatcherX")
   val start = System.currentTimeMillis()
@@ -33,19 +34,67 @@ object SentenceMatcherX extends SentenceMatcherXSparkApp {
 
     val sentences = spark.sql(
       s"""| SELECT fileName, inline(sentences) FROM documents
+          |
           | HAVING sentenceLength BETWEEN $MIN_SENTENCE_LENGTH AND $MAX_SENTENCE_LENGTH
-          | """.stripMargin)
+          | """.stripMargin) // WHERE fileName LIKE '%novica%'
 
     logger.info(s"Sentences count is ${sentences.count()}.")
 
-    val sentencesRDD = LuceneRDD(
-      sentences.select('fileName, 'pageNumber, 'sentenceIndex, 'sentenceLength, 'sentence))
+    val rawSentencesRDD = sentences.select('fileName, 'pageNumber, 'sentenceIndex, 'sentenceLength, 'sentence)
+
+    val sentencesRDD = LuceneRDD(rawSentencesRDD)
     sentencesRDD.cache()
 
-    logger.info(s"LuceneRDD count: ${sentencesRDD.count()}")
+    val luceneRDDCount: Long = sentencesRDD.count()
+    logger.info(s"LuceneRDD count: ${luceneRDDCount}")
 
-    val results = sentencesRDD.termQuery("sentence", "diploma", 10)
+    println("~*" * 40)
+
+    val results = sentencesRDD.termQuery("sentence", "pogodba", 10)
     results.foreach(println)
+
+    println("~*" * 40)
+
+    val rowLinker: Row => String = {
+      case row => {
+        val fileName = row.getString(0)
+        val sentence = row.getString(4)
+        s"""(sentence:"$sentence"~$SENTENCE_FUZZINESS) AND !(fileName:"$fileName")"""
+      }
+    }
+
+    val linked = sentencesRDD.linkDataFrame(rawSentencesRDD, rowLinker, luceneRDDCount.toInt)
+
+    /*
+    val linkageResults = spark.createDataFrame(linkedResults.filter(_._2.nonEmpty).map{ case (acm, topDocs) => (topDocs.head.doc.textField("id").head, acm.getInt(acm.fieldIndex("id")).toString)})
+      .toDF("idDBLP", "idACM")
+      */
+
+
+    val linkedResults = linked.filter(_._2.nonEmpty)
+
+    // linkedResults.foreach(println)
+
+
+    linkedResults.take(10).foreach {
+      case (row, docs) => {
+        println("\n" * 1)
+        println("#" * 40)
+
+        val fileName = row.getString(0)
+        val sentence = row.getString(4)
+        println(s"[$fileName] $sentence")
+        println("-" * 20)
+
+        docs.foreach(sparkScoreDoc => {
+          val score = sparkScoreDoc.score
+          val sentence = sparkScoreDoc.doc.textField("sentence").getOrElse("[nothing]")
+          val fileName = sparkScoreDoc.doc.textField("fileName").getOrElse("[nothing]")
+          println(s"--> $fileName [$score] $sentence")
+        })
+      }
+    }
+
 
   } finally {
     val end = System.currentTimeMillis()
